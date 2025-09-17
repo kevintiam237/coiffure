fa5b326cd414f440fb83a0a57e2273a2fb81a6ec
kevintiam237 <tiamkevin9@gmail.com>
2025-09-17 05:00:42 -0400

    mise en place des route pour le local

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+    proxy: {
+      '/api': 'http://localhost:3005'
+    }
+  },
   base: './',
   plugins: [
     tailwindcss(),
