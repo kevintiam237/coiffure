9a3aff2114d90f87a634add23740de7b6b094c6b
kevintiam237 <tiamkevin9@gmail.com>
2025-09-17 04:24:37 -0400

    02

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://elgancecoiffure-six.vercel.app"
+}));
+
 app.use(express.json());
 app.use(express.static("public"));
 
